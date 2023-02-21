c95b0050a7eb2db594bcc31e028f6beecabd3391
Melis Akinci <melis.akinci8@gmail.com>
2023-02-20 19:19:38 -0500

    outline

---
+++ main.py
@@ -0,0 +1,22 @@
+#connect to mongo database
+#TODO: get one programatically run query that outputs a visualization
+
+#query 1 - basic
+
+#query 2 - basic
+
+#query 3 - basic
+
+#query 4 - basic
+
+#query 5 - basic
+
+#query 6 - intermediate
+
+#query 7 - intermediate
+
+#query 8 - intermediate
+
+#query 9 - hard
+
+#query 10 - hard
