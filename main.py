afd4abd4ce8c904f673b8797064ed05074a04376
Melis Akinci <melis.akinci8@gmail.com>
2023-02-22 20:54:16 -0500

    FIXED YAY

--- main.py
+++ main.py
@@ -1,104 +1,103 @@
 #connect to mongo database
-#TODO: get one programatically run query that outputs a visualization --> in python
+#TODO: get one programatically run query that outputs a visualization --> in python --> pref. intermediate or hard
+#submit python code with just this query
 '''
 extract data from mongo --> use PyMongo to make that visualization, matplotlib, etc,
 '''
+#TODO: export data to google doc, intermediate query #7
 
-import pymongo
-
-# connect to MongoDB
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["Restaurants"]
-col = db["Restaurants"]
+#3 things you need:
+#requirement and expectations of the query
+#actual mongo query --> parameter, count, best grades, etc
+#result of query
 
 from pymongo import MongoClient
+from datetime import datetime
+from pprint import pprint
+import random as rnd
+import time
 
-# connect to the database
+#connecting to the Mongo
 client = MongoClient()
-db = client['restaurants']
+db = client.HW4
 
-# EASY QUERIES
 
-# 1. Find all restaurants in a specific borough (e.g. "Bronx")
-borough = "Bronx"
-result = db.restaurants.find({"borough": borough})
-for r in result:
-    print(r)
+#query 1 - basic
+#what are the first 10 names of all the restaurants in the Bronx
+result = db.Restaurant.distinct("name", {"borough": "Bronx"})
+#print(result)
 
-# 2. Find all restaurants that have received a specific grade (e.g. "B")
-grade = "B"
-result = db.restaurants.find({"grades.grade": grade})
-for r in result:
-    print(r)
 
-# 3. Find all restaurants that serve a specific cuisine (e.g. "Italian")
-cuisine = "Italian"
-result = db.restaurants.find({"cuisine": cuisine})
-for r in result:
-    print(r)
+#query 2 - basic
+#what are the names of all the Chinese restaurants in Queens that have a score of 50 or higher?
 
-# 4. Find all restaurants that have a score above a certain threshold (e.g. 20)
-threshold = 20
-result = db.restaurants.find({"grades.score": {"$gt": threshold}})
-for r in result:
-    print(r)
+query = {"borough": "Queens", "cuisine": "Chinese", "grades.score": {"$gte": 50}}
+projection = {"name": 1, "_id": 0}
+result = list(db.Restaurant.find(query, projection))
+#print(result)
 
-# 5. Find all restaurants that are located on a specific street (e.g. "Flatbush Avenue")
-street = "Flatbush Avenue"
-result = db.restaurants.find({"address.street": street})
-for r in result:
-    print(r)
+#query 3 - basic
+#how many restaurants are there in each borough?
+pipeline = [{"$group": {"_id": "$borough", "count": {"$sum": 1}}}]
+result = list(db.Restaurant.aggregate(pipeline))
+#print(result)
 
-# INTERMEDIATE QUERIES
+#query 4 - basic
+#what is the average score for all restaurants in the dataset?
 
-# 6. Find the top 5 restaurants with the highest average score
-result = db.restaurants.aggregate([
+pipeline = [{"$unwind": "$grades"}, {"$group": {"_id": None, "avg_score": {"$avg": "$grades.score"}}}]
+result = list(db.Restaurant.aggregate(pipeline))
+#print(result)
+
+
+#query 5 - basic
+#what are the top 10 cuisines based on number of restaurats int he dataset?
+
+pipeline = [{"$group": {"_id": "$cuisine", "count": {"$sum": 1}}}, {"$sort": {"count": -1}}, {"$limit": 10}]
+
+result = list(db.Restaurant.aggregate(pipeline))
+#print(result)
+
+#query 6 - intermediate
+#find the distinct cuising types in the restaurants collection
+
+cuisines = db.Restaurant.distinct("cuisine")
+#print(cuisines)
+
+
+#query 7 - intermediate
+#find the count of restaurants that have a cuisine of Italian and are located in Manhattan borough
+count = db.Restaurant.count_documents({"borough": "Manhattan", "cuisine": "Italian"})
+#print(count)
+
+
+
+#query 9 - hard
+#what are the top 10 restaurants with the highest average grade scores for all their grades?
+
+top_restaurants = db.Restaurant.aggregate([
     {"$unwind": "$grades"},
-    {"$group": {"_id": "$_id", "avg_score": {"$avg": "$grades.score"}}},
-    {"$sort": {"avg_score": -1}},
+    {"$group": {
+        "_id": "$_id",
+        "name": {"$first": "$name"},
+        "average_score": {"$avg": "$grades.score"}
+    }},
+    {"$sort": {"average_score": -1}},
+    {"$limit": 10}
+])
+#for restaurant in top_restaurants:
+   # print(restaurant)
+
+
+
+#query 10 - hard
+#what are the 5 most common cuisine types in Manhattan and their respective counts?
+
+manhattan_cuisine_counts = db.Restaurant.aggregate([
+    {"$match": {"borough": "Manhattan"}},
+    {"$group": {"_id": "$cuisine", "count": {"$sum": 1}}},
+    {"$sort": {"count": -1}},
     {"$limit": 5}
 ])
-for r in result:
-    restaurant = db.restaurants.find_one({"_id": r["_id"]})
-    print(restaurant)
-
-# 7. Find the borough with the highest number of restaurants
-result = db.restaurants.aggregate([
-    {"$group": {"_id": "$borough", "count": {"$sum": 1}}},
-    {"$sort": {"count": -1}},
-    {"$limit": 1}
-])
-for r in result:
-    print("Borough with highest number of restaurants:", r["_id"])
-
-# 8. Find the number of restaurants in each borough
-result = db.restaurants.aggregate([
-    {"$group": {"_id": "$borough", "count": {"$sum": 1}}},
-    {"$sort": {"_id": 1}}
-])
-for r in result:
-    print("Borough:", r["_id"], "Count:", r["count"])
-
-# HARD QUERIES
-
-# 9. Find the restaurant with the highest number of "A" grades
-result = db.restaurants.aggregate([
-    {"$unwind": "$grades"},
-    {"$match": {"grades.grade": "A"}},
-    {"$group": {"_id": "$_id", "count": {"$sum": 1}}},
-    {"$sort": {"count": -1}},
-    {"$limit": 1}
-])
-for r in result:
-    restaurant = db.restaurants.find_one({"_id": r["_id"]})
-    print(restaurant)
-
-# 10. Find the restaurant with the highest number of "C" or "D" grades and a score below 10
-result = db.restaurants.aggregate([
-    {"$unwind": "$grades"},
-    {"$match": {"$and": [{"grades.grade": {"$in": ["C", "D"]}}, {"grades.score": {"$lt": 10}}]}},
-    {"$group": {"_id": "$name", "count": {"$sum": 1}}},
-    {"$sort": {"count": -1}},
-    {"$limit": 1}
-])
-
+for cuisine_count in manhattan_cuisine_counts:
+    print(cuisine_count)
